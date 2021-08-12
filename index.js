d7a963c3899377d3c870d0893d8f42a9a179328c
John Hutchison <jhutchison@speareducation.com>
2021-08-11 23:46:00 -0700

    [GL-1972] All examples complete

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const consola = require('consola');
+const { DateTime, Duration } = require('luxon')
+const moment = require('moment-timezone');
+const { format, utcToZonedTime } = require('date-fns-tz');
+const { add, formatDistance, formatRelative } = require('date-fns');
 
 // Simple test assert with a message
 function assert(message, condition, stack = false) {
@@ -25,7 +29,7 @@ function section(title) {
 }
 
 // Constants
-const MySQL_Date = '2021-06-30 01:00:00';
+const MySQL_Date = '2021-07-01 01:00:00';
 const ISO8601_Date = mysqlToISO(MySQL_Date);
 const timeZone = 'America/Phoenix';
 
@@ -82,7 +86,7 @@ assert(
 );
 assert(
     `Default date format in ${timeZone} should be correct`,
-    jsDate.toLocaleString('en-US', { timeZone }) === '6/29/2021, 6:00:00 PM',
+    jsDate.toLocaleString('en-US', { timeZone }) === '6/30/2021, 6:00:00 PM',
 );
 
 // Intl.DateTimeFormat should be interchangable with the Date.toLocaleString(), but Intl.DateTimeFormat is reusable
@@ -124,6 +128,140 @@ consola.info('Relative dates:', {
     'after now?': jsDate30Days > Date.now(),
 });
 
-// Using Moment.js
+/**
+ * Using Moment.js
+ * For comparison only
+ */
 
+section('Using Moment.js');
 
+const momentDate = moment(ISO8601_Date);
+
+consola.info('Parsed:', {
+    momentDate,
+    ISO: momentDate.toISOString(),
+});
+
+assert(
+    'Parsed MySQL date as UTC should match parsed ISO date',
+    momentDate.isSame(moment.utc(MySQL_Date)),
+);
+assert(
+    'Parsed ISO date should match input ISO date',
+    momentDate.toISOString() === ISO8601_Date,
+);
+assert(
+    `Default date format in ${timeZone} should be correct`,
+    momentDate.tz(timeZone).format() === '2021-06-30T18:00:00-07:00',
+);
+
+consola.info(`Formatting examples for ${timeZone}:`, {
+    'default datetime': momentDate.tz(timeZone).format(),
+    'dddd, MMMM Do YYYY, h:mm:ss a': momentDate.tz(timeZone).format('dddd, MMMM Do YYYY, h:mm:ss a'),
+    'LLL': momentDate.tz(timeZone).format('LLL'),
+});
+
+/**
+ * Using Luxon
+ */
+
+section('Using Luxon');
+
+const luxonDate = DateTime.fromISO(ISO8601_Date, { zone: 'UTC' }); // must be in ISO-8601
+const luxonDateZoned = luxonDate.setZone(timeZone);
+
+consola.info('Parsed:', {
+    luxonDate,
+    ISO: luxonDate.toString(),
+});
+
+assert(
+    'Parsed ISO date should be the same timestamp as localized default',
+    // Luxon caveat, .equals() is only true if the zone is also the same
+    // luxonDate.equals(DateTime.fromISO(ISO8601_Date)), ==> false
+    +luxonDate === +(DateTime.fromISO(ISO8601_Date)),
+);
+assert(
+    'Parsed MySQL date as formatted UTC should match parsed ISO date',
+    +luxonDate === +(DateTime.fromFormat(MySQL_Date, 'yyyy-MM-dd HH:mm:ss', { zone: 'UTC' })),
+);
+assert(
+    'Parsed ISO date should match input ISO date',
+    luxonDate.toISO() === ISO8601_Date,
+);
+assert(
+    `Default date format in ${timeZone} should be correct`,
+    luxonDateZoned.toFormat('yyyy-MM-dd HH:mm:ss') === '2021-06-30 18:00:00',
+);
+
+consola.info(`Formatting examples for ${timeZone}:`, {
+    'dddd, MMMM Do YYYY, h:mm:ss a': luxonDateZoned.toFormat('dddd, MMMM Do YYYY, h:mm:ss a'),
+    'LLL d, yyyy': luxonDateZoned.toFormat('LLL d, yyyy'),
+});
+
+// Relative dates
+const duration30Days = Duration.fromObject({ days: 30 });
+const luxonDate30Days = luxonDate.plus(duration30Days);
+// OR
+// const luxonDate30Days = luxonDate.plus({ days: 30 });
+
+assert(
+    'Relative date is after (larger than) original date',
+    luxonDate30Days > luxonDate,
+);
+
+consola.info('Relative dates:', {
+    luxonDate30Days: luxonDate30Days.toFormat('yyyy-MM-dd HH:mm:ss'),
+    'duration30Days in hours': duration30Days.as('hours'),
+    'after now?': luxonDate30Days > Date.now(),
+});
+
+/**
+ * Using date-fns
+ */
+
+section('Using date-fns-tz');
+
+const fnsDate = new Date(ISO8601_Date); // date-fns uses native Date objects
+const fnsDateZoned = utcToZonedTime(fnsDate, timeZone);
+
+consola.info('Parsed:', {
+    fnsDate,
+    ISO: fnsDate.toISOString(),
+});
+
+assert(
+    'Parsed ISO date should be the same timestamp as localized',
+    +fnsDate === +fnsDateZoned,
+);
+assert(
+    'Parsed ISO date should match input ISO date',
+    fnsDate.toISOString() === ISO8601_Date,
+);
+assert(
+    `Default date format in ${timeZone} should be correct`,
+    format(fnsDateZoned, 'yyyy-MM-dd HH:mm:ss') === '2021-06-30 18:00:00',
+);
+assert(
+    `Formatted zoned date should match formatted UTC date with zone`,
+    format(fnsDateZoned, 'yyyy-MM-dd HH:mm:ss') === format(fnsDate, 'yyyy-MM-dd HH:mm:ss', { timeZone }),
+);
+
+consola.info(`Formatting examples for ${timeZone}:`, {
+    'd.M.yyyy HH:mm:ss.SSS GMT XXX (z)': format(fnsDateZoned, 'd.M.yyyy HH:mm:ss.SSS GMT XXX (z)'),
+    'LLL d, yyyy': format(fnsDateZoned, 'LLL d, yyyy'),
+});
+
+// Relative dates
+const fnsDate30Days = add(fnsDate, { days: 30 });
+
+assert(
+    'Relative date is after (larger than) original date',
+    fnsDate30Days > fnsDate,
+);
+
+consola.info('Relative dates:', {
+    fnsDate30Days: format(fnsDate30Days, 'LLL d, yyyy'),
+    relative: formatRelative(fnsDate30Days, fnsDate30Days),
+    duration: formatDistance(fnsDate30Days, fnsDate),
+});
